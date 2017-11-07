27d6833aeb7977e360f1df63512c8702775bd781
opiumfive <opiumfive@gmail.com>
2017-11-07 20:06:29 +0300

    some fixes

--- app/src/main/java/com/opiumfive/smtest/Contact.java
+++ app/src/main/java/com/opiumfive/smtest/Contact.java
@@ -4,12 +4,18 @@ package com.opiumfive.smtest;
 public class Contact {
 
     private String name;
-    private String phone;
     private String email;
 
-    public Contact(String name, String phone, String email) {
+    public Contact(String name, String email) {
         this.name = name;
-        this.phone = phone;
         this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
--- app/src/main/java/com/opiumfive/smtest/GetContsTask.java
+++ app/src/main/java/com/opiumfive/smtest/GetContsTask.java
@@ -1,11 +1,13 @@
 package com.opiumfive.smtest;
 
 
+import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.provider.ContactsContract;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -29,7 +31,7 @@ public class GetContsTask extends AsyncTask<Void, Void, List<Contact>> {
         List<Contact> contList = new ArrayList<>();
 
         if (activity != null) {
-            Cursor people = null;
+            /*Cursor people = null;
             try {
                 people = activity.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
                 while (people.moveToNext()) {
@@ -62,7 +64,39 @@ public class GetContsTask extends AsyncTask<Void, Void, List<Contact>> {
                 }
             }
 
-            return contList;
+            return contList; */
+
+            HashSet<String> emailsSet = new HashSet<>();
+
+            ContentResolver cr = activity.getContentResolver();
+            String[] PROJECTION = new String[] {
+                    ContactsContract.RawContacts._ID,
+                    ContactsContract.Contacts.DISPLAY_NAME,
+                    ContactsContract.Contacts.PHOTO_ID,
+                    ContactsContract.CommonDataKinds.Email.DATA,
+                    ContactsContract.CommonDataKinds.Photo.CONTACT_ID };
+            String order = "CASE WHEN "
+                    + ContactsContract.Contacts.DISPLAY_NAME
+                    + " NOT LIKE '%@%' THEN 1 ELSE 2 END, "
+                    + ContactsContract.Contacts.DISPLAY_NAME
+                    + ", "
+                    + ContactsContract.CommonDataKinds.Email.DATA
+                    + " COLLATE NOCASE";
+            String filter = ContactsContract.CommonDataKinds.Email.DATA + " NOT LIKE ''";
+            Cursor cur = cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, PROJECTION, filter, null, order);
+            if (cur.moveToFirst()) {
+                do {
+                    String name = cur.getString(1);
+                    String emailAddress = cur.getString(3);
+
+                    // keep unique only
+                    if (emailsSet.add(emailAddress.toLowerCase())) {
+                        contList.add(new Contact(name, emailAddress));
+                    }
+                } while (cur.moveToNext());
+            }
+
+            cur.close();
         }
         return contList;
     }
